8244a093d6cc65be0598df4cbd4321249fa3256d
pineapplechunks <51415258+Spunkdunk@users.noreply.github.com>
2019-10-31 18:32:55 +0100

    working on ProductDao

--- src/main/java/no/kristiania/jdbc/ProductDao.java
+++ src/main/java/no/kristiania/jdbc/ProductDao.java
@@ -1,17 +1,53 @@
 package no.kristiania.jdbc;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao {
 
     private List<String> products = new ArrayList <>();
+    private DataSource dataSource;
+
+    public ProductDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public void insertProduct(String productName) {
         products.add(productName);
+
+        try { Connection conn = dataSource.getConnection();
+            try (PreparedStatement statement = conn.prepareStatement("insert into products (name) values (?)")) {
+                statement.setString(1, productName);
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<String> listAll() {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from products")) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    List<String> result = new ArrayList<>();
+
+                    while (rs.next()) {
+                        result.add(rs.getString("name"));
+                    }
+
+                    return result;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return products;
+
+
     }
 }
--- src/test/java/no/kristiania/jdbc/WebshopTest.java
+++ src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -1,7 +1,9 @@
 package no.kristiania.jdbc;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,8 +11,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WebshopTest {
 
     @Test
-    void shouldReturnStoredProduct() {
-        ProductDao dao = new ProductDao();
+    void shouldReturnStoredProduct() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test");
+
+        dataSource.getConnection().createStatement().executeUpdate(
+                "create table products (name varchar(100))"
+        );
+
+        ProductDao dao = new ProductDao(dataSource);
         String productName = pickOne(new String[] {"Apples", "Bananas", "Coconuts", "Dates"});
         dao.insertProduct(productName);
         assertThat(dao.listAll()).contains(productName);
